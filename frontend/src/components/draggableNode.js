5177fc52c8ae25260562945973c2468a6d120a8d
RutamBhagat <rutambhagat@gmail.com>
2025-01-01 18:59:14 +0530

    feat(draggableNode): replace div with Button component for improved styling and interaction; update drag event handling

--- frontend/src/components/draggableNode.js
+++ frontend/src/components/draggableNode.js
@@ -1,19 +1,25 @@
+import { Button } from "./ui/button";
+
 export const DraggableNode = ({ type, label }) => {
-    const onDragStart = (event, nodeType) => {
-      const appData = { nodeType }
-      event.target.style.cursor = 'grabbing';
-      event.dataTransfer.setData('application/reactflow', JSON.stringify(appData));
-      event.dataTransfer.effectAllowed = 'move';
-    };
-  
-    return (
-      <div
-        className={`${type} min-w-[80px] h-[60px] flex items-center justify-center flex-col rounded-lg bg-[#1C2536] cursor-grab`}
-        onDragStart={(event) => onDragStart(event, type)}
-        onDragEnd={(event) => (event.target.style.cursor = 'grab')}
-        draggable
-      >
-          <span className="text-white">{label}</span>
-      </div>
+  const onDragStart = (event, nodeType) => {
+    const appData = { nodeType };
+    event.target.style.cursor = "grabbing";
+    event.dataTransfer.setData(
+      "application/reactflow",
+      JSON.stringify(appData)
     );
+    event.dataTransfer.effectAllowed = "move";
   };
+
+  return (
+    <Button
+      variant="secondary"
+      className={`${type} min-w-[80px] h-[50px] flex items-center justify-center text-white hover:bg-gray-700 flex-col rounded-lg bg-[#1C2536] cursor-grab`}
+      onDragStart={(event) => onDragStart(event, type)}
+      onDragEnd={(event) => (event.target.style.cursor = "grab")}
+      draggable
+    >
+      {label}
+    </Button>
+  );
+};
